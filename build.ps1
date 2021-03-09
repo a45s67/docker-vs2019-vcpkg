e0e2fb0c27689c31a650ac21ea6239578fbebb02
fish Yu <b45s67@gmail.com>
2021-03-09 12:15:29 +0800

    build order : choco->vcpkg_dir->vs2019->vcpkg

--- build.ps1
+++ build.ps1
@@ -1,6 +1,9 @@
 cd chocolatey
 docker build -t chocolatey .
-cd ../vs2019
-docker build -t vs2019 .
 cd ../vcpkg_dir
 docker build -t vcpkg_dir .
+cd ../vs2019
+docker build -t vs2019 .
+
+cd ../vcpkg
+docker build -t vcpkg .
